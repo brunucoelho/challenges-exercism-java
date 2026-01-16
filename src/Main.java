fac26925bc02bf998bd732f40bc08e3bbaf3c2e7
brunucoelho <bruno-coelho@hotmail.com>
2026-01-15 21:59:55 -0300

    feat:finalizacao do exercicio ResistorColor

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        HighScores highScores = new HighScores(Arrays.asList(70, 50, 20, 30, 90));
-        //highScores.personalTopThree();
-        System.out.println(highScores.personalBest());
-        System.out.println(highScores.personalTopThree());
-        System.out.println(highScores.latest());
+        // HighScores highScores = new HighScores(Arrays.asList(70, 50, 20, 30, 90));
+        // //highScores.personalTopThree();
+        // System.out.println(highScores.personalBest());
+        // System.out.println(highScores.personalTopThree());
+        // System.out.println(highScores.latest());
+
+        ResistorColor resistorColor = new ResistorColor();
+        System.out.println(resistorColor.colorCode("blue"));
+                System.out.println(Arrays.toString(resistorColor.colors()));
+                
     }
 }
--- src/ResistorColor.java
+++ src/ResistorColor.java
@@ -1,9 +1,25 @@
 class ResistorColor {
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int value = 0;
+        switch(color) {
+                case("black") -> value = 0;
+                case("brown") -> value = 1;
+                case("red") -> value = 2;
+                case("orange") -> value = 3;
+                case("yellow") -> value = 4;
+                case("green") -> value = 5;
+                case("blue") -> value = 6;
+                case("violet") -> value = 7;
+                case("grey") -> value = 8;
+                case("white") -> value = 9;
+        };
+    return value;
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new String[] {
+            "black", "brown", "red", "orange", "yellow",
+            "green", "blue", "violet", "grey", "white"
+        };
     }
 }
