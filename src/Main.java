c7a7e95bfd10abc499fc7babcb5d2eea9fbc2858
brunucoelho <bruno-coelho@hotmail.com>
2026-02-03 22:46:17 -0300

    feat:Finalização do exercicio Hamming

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
+import bm_Hamming.Hamming;
+
 public class Main {
     public static void main(String[] args) {
-        String out = "";
-        String input = "The Road _Not_ Taken";
-        String[] inputSplit = input.split("[\\p{Zs}-_]+");
-        for (String str : inputSplit) {
-            out += str.toUpperCase().charAt(0);
-        }
-        System.out.println(out);
-        
-        // Acronym acronym = new Acronym("Liquid-crystal display");
-        // System.out.println(acronym.get());
+        Hamming hamming = new Hamming("AATG", "AAA");
+        int distance = hamming.getHammingDistance();
+        System.out.println(distance);
     }
 };
