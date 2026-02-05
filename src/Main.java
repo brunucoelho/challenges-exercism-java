a48d5b44c842c07712e6390665ba601e69b18b4e
brunucoelho <bruno-coelho@hotmail.com>
2026-02-05 12:44:57 -0300

    feat:finalizacao do exercicio Pangram

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import bm_Hamming.Hamming;
 
 public class Main {
     public static void main(String[] args) {
-MicroBlog microBlog = new MicroBlog();
-        String expected = "Fly 🛫";
-        String result = microBlog.truncate(expected);
-        System.out.println(result);
+        PangramChecker pangramChecker = new PangramChecker();
+        System.out.println(pangramChecker.isPangram("Th quick brown fox jumps ovr th lazy dog"));
+
     }
 };
---
+++ src/PangramChecker.java
@@ -0,0 +1,13 @@
+public class PangramChecker {
+
+    int sum = 0;
+
+    public boolean isPangram(String input) {
+        String test = "abcdefghijklmnopqrstuvxzyw";
+        input = input.toLowerCase();
+        for (int i = 0; i < test.length(); i++) {
+            if (test.charAt(i) >= 'a' && test.charAt(i) <= 'z' && input.indexOf(test.charAt(i)) != -1)
+                sum++;
+        }
+        return (sum == 26) ? true : false;
+}}
